a25d1d9007f1e863353a15d7ad4dc4c4b40753e8
Oudart Matéo <mateooudart@gmail.com>
2023-10-22 20:46:23 +0200

    added unit test

--- games/categorie/tests.py
+++ games/categorie/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import CategoryModel
 
-# Create your tests here.
+class CategoryModelTests(APITestCase):
+
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(username='adminTest', password='azerty123456', email='admintest@test.com')
+        self.client.force_authenticate(user=self.superuser)
+
+        self.category_data = {
+            'name': 'Test Category Name'
+        }
+
+        self.test_category = CategoryModel.objects.create(**self.category_data)
+
+    # GET CATEGORIES - TEST UNITAIRE
+    def test_list_categories(self):
+        response = self.client.get('/categorie/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.data), 0)
+
+    # POST CATEGORY - TEST UNITAIRE
+    def test_create_category(self):
+        response = self.client.post('/categorie/', self.category_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CategoryModel.objects.count(), 2)
+
+        self.assertEqual(response.data['name'], 'Test Category Name')
+
+    # PATCH CATEGORY - TEST UNITAIRE
+    def test_update_category(self):
+        updated_data = {'name': 'Updated Category Name'}
+        response = self.client.patch(f'/categorie/{self.test_category.id}/', updated_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.test_category.refresh_from_db()
+        self.assertEqual(self.test_category.name, 'Updated Category Name')
+
+    # DELETE CATEGORY - TEST UNITAIRE
+    def test_destroy_category(self):
+        response = self.client.delete(f'/categorie/{self.test_category.id}/')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(CategoryModel.DoesNotExist):
+            CategoryModel.objects.get(id=self.test_category.id)
+
+    def test_destroy_nonexistent_category(self):
+        response = self.client.delete('/categorie/9999/')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- games/gameApi/tests.py
+++ games/gameApi/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from games.gameApi.models import GameModel
+from games.categorie.models import CategoryModel
 
 # Create your tests here.
+class GameViewSetTests(APITestCase):
+
+  def setUp(self):
+    self.superuser = User.objects.create_superuser(username='adminTest', password='azerty123456', email='admintest@test.com')
+    categorie = CategoryModel.objects.create(name='TEST')
+
+    self.client.force_authenticate(user=self.superuser)
+
+    self.game_data = {
+      'title': 'title',
+      'desc': 'description',
+      'addedDate': '2023-10-20',
+      'categorie': categorie,
+      'plateform': 'game plateforme'
+    }
+
+    self.test_game = GameModel.objects.create(**self.game_data)
+
+  #GET GAMES - TEST UNITAIRE
+  def test_list_posts(self):
+    response = self.client.get('/games/')
+
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    self.assertGreater(len(response.data), 0)
+
+  def test_create_game(self):
+    url = '/games/'
+    response = self.client.post(url, self.game_data, format='json')
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    self.assertEqual(GameModel.objects.count(), 2)
